f4f6697cc58c8c9da22c63f6b94c33ca10a602c2
Drew Davis <kalaborative94@gmail.com>
2018-05-02 17:51:46 -0500

    add OpenSSL cert

--- app.py
+++ app.py
@@ -253,5 +253,5 @@ if __name__ == "__main__":
 	# convention to run on Heroku
 	port = int(environ.get("PORT", 5000))
 	# run the app available anywhere on the network, on debug mode
-	app.run(host="0.0.0.0", port=port, debug=True)
+	app.run(host="0.0.0.0", port=port, debug=True, ssl_context=('chained.pem', 'private.key'))
 	# app.run(debug=True)
