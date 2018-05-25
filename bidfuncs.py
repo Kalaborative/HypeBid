018fab736bff77a0590cfe578890e54726dc0da4
Drew Davis <kalaborative94@gmail.com>
2018-05-25 10:57:10 -0500

    Initial bidding commit

---
+++ bidfuncs.py
@@ -0,0 +1,90 @@
+from pymongo import MongoClient
+from collections import Counter
+
+def listAllItems():
+	"""Returns a list of all items."""
+	client = MongoClient("mongodb+srv://HBDB_User:DTfjUidPbZfAhdlF@hypebiddb-xkxgt.mongodb.net/test")
+	data = client.hypeBidDB.items
+	results = []
+	for item in data.find():
+		results.append(item['item_id'])
+	return results
+
+def addItem(newItemId):
+	"""Adds a new item with item ID starting with item_."""
+	client = MongoClient("mongodb+srv://HBDB_User:DTfjUidPbZfAhdlF@hypebiddb-xkxgt.mongodb.net/test")
+	data = client.hypeBidDB.items
+	newItemDict = {"item_id": newItemId, 'users_bidding': []}
+	data.insert_one(newItemDict)
+	return True
+
+def getValidItemIDs():
+	client = MongoClient("mongodb+srv://HBDB_User:DTfjUidPbZfAhdlF@hypebiddb-xkxgt.mongodb.net/test")
+	data = client.hypeBidDB.items
+	validItems = data.find()
+	item_ids = []
+	for item in validItems:
+		item_ids.append(item['item_id'])
+	return item_ids
+
+def addBid(chosenItem, username, bid):
+	"""Adds a bid to them item given its item ID, username, and bid amount."""
+	validgameIDs = getValidItemIDs()
+	if chosenItem in validgameIDs:
+		match = False
+		biddingList = [i['users_bidding'] for i in data.find() if i['item_id'] == chosenItem][0]
+		for b in biddingList:
+			if b['user_name'] == username:
+				match = True
+		try:
+			bid = float(bid)
+			if match:
+				print("This person already has a bet. This will overwrite the original bet.")
+				data.update_one({'item_id': chosenItem, 'users_bidding.user_name': username}, {"$set": {'users_bidding.$.user_bid': bid}})
+				return True
+			else:
+				newBidData = {"user_name": username, "user_bid": bid}
+				data.update_one({'item_id': chosenItem}, {"$push": {"users_bidding": newBidData}})
+				return True
+		except:
+			print("You entered an invalid bid. Please enter it in a decimal form.")
+			return False
+	else:
+		print("That item ID is invalid. Please try again.")
+		return False
+
+def listItemBids():
+	"""Returns a dict of all bids on each item."""
+	client = MongoClient("mongodb+srv://HBDB_User:DTfjUidPbZfAhdlF@hypebiddb-xkxgt.mongodb.net/test")
+	data = client.hypeBidDB.items
+	results = {}
+	for d in data.find():
+		results[d['item_id']] = d['users_bidding']
+	return results
+
+def clearAllBids():
+	"""Removes all bids on all items."""
+	client = MongoClient("mongodb+srv://HBDB_User:DTfjUidPbZfAhdlF@hypebiddb-xkxgt.mongodb.net/test")
+	data = client.hypeBidDB.items
+	data.update_many({}, {"$set": {"users_bidding": []}})
+	return True
+
+def calculate_winning_bid(chosenItem):
+	"""Returns the user with the winning bid given a valid Item ID."""
+	validgameIDs = getValidItemIDs()
+	if chosenItem in validgameIDs:
+		biddingList = [i['users_bidding'] for i in data.find() if i['item_id'] == chosenItem][0]
+		dataDict = {}
+		for b in biddingList:
+			dataDict[b['user_name']] = b['user_bid']
+
+		c = Counter(dataDict.values())
+		winningBid = [item for item, count in Counter(c).items() if count == 1]
+
+		for a, b in dataDict.items():
+			if b == min(winningBid):
+				winresults = "Winner is {}.".format(a)
+		return winresults
+	else:
+		print("Item ID is not valid. Please try again.")
+		return False
