33fe0fd028dbc1929b899e1ab1bf938cce1b0efd
Drew Davis <kalaborative94@gmail.com>
2018-05-27 07:57:18 -0500

    Check AWS keys

--- s3.py
+++ s3.py
@@ -1,7 +1,11 @@
 import boto3
 from credentials import access_key, secret_key, region
+from os import environ
+
 
 def uploadToS3(filename):
+	print(environ['AWS_ACCESS_KEY_ID'])
+	print(environ['AWS_SECRET_ACCESS_KEY'])
 	session = boto3.Session(aws_access_key_id=access_key, aws_secret_access_key=secret_key, region_name=region)
 
 	s3 = session.resource('s3')
