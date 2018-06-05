435e69ef57c4135b9f911b7b3bbb0937d23e790a
Drew Davis <kalaborative94@gmail.com>
2018-06-05 13:29:57 -0500

    Fixed typo in bucket name

--- s3.py
+++ s3.py
@@ -8,6 +8,6 @@ def uploadToS3(filename):
 
 	s3 = session.resource('s3')
 	data = open('itemUploads/{}'.format(filename), 'rb')
-	s3.Bucket('hypebid-uploads').put_object(Key=filename, Body=data)
+	s3.Bucket('hypebid-upload').put_object(Key=filename, Body=data)
 	url = 'https://s3.us-east-2.amazonaws.com/hypebid-upload/{}'.format(filename)
 	return url
