d725a90d7efbe0fed269aebab237f8fc4c4ad487
Drew Davis <kalaborative94@gmail.com>
2018-06-20 13:13:05 -0500

    Remove file from temp folder after upload

--- s3.py
+++ s3.py
@@ -1,4 +1,5 @@
 import boto3
+from os import remove
 from retrieveCreds import getCredentials
 
 
@@ -7,7 +8,8 @@ def uploadToS3(filename):
 	session = boto3.Session(aws_access_key_id=user['access_key'], aws_secret_access_key=user['secret_key'], region_name='us-east-2')
 
 	s3 = session.resource('s3')
-	data = open('itemUploads/{}'.format(filename), 'rb')
-	s3.Bucket('hypebid-upload').put_object(Key=filename, Body=data)
-	url = 'https://s3.us-east-2.amazonaws.com/hypebid-upload/{}'.format(filename)
+	with open('itemUploads/{}'.format(filename), 'rb') as data:
+		s3.Bucket('hypebid-upload').put_object(Key=filename, Body=data)
+		url = 'https://s3.us-east-2.amazonaws.com/hypebid-upload/{}'.format(filename)
+	remove("itemUploads/{}".format(filename))
 	return url
